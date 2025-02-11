653cec896ca7d8a46bf425753b167bc14cea2cf3
Clarissa Lopez <lopezclarissa2021@gmail.com>
2025-02-10 18:00:31 -0800

    Added category controller and started order model

---
+++ TrilumeCandles/Models/Order.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TrilumeCandles.Models
+{
+    public class Order
+    {
+        [Key]
+        public int Id { get; set; }
+        public string UserId { get; set; }
+        public string Status { get; set; }
+        public decimal Total { get; set; }
+        public DateTime DateTime { get; set; }
+    }
+}
