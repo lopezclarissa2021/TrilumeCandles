411e98f92c9f0ed1dca479f118e31a3e19a5e186
Clarissa Lopez <lopezclarissa2021@gmail.com>
2025-02-10 18:15:00 -0800

    Added more to the order table

--- TrilumeCandles/Models/Order.cs
+++ TrilumeCandles/Models/Order.cs
@@ -7,8 +7,12 @@ namespace TrilumeCandles.Models
         [Key]
         public int Id { get; set; }
         public string UserId { get; set; }
-        public string Status { get; set; }
+        public string OrderStatus { get; set; }
+        public string ShippingAddress { get; set; }
         public decimal Total { get; set; }
-        public DateTime DateTime { get; set; }
+        public DateTime OrderDate { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; } = DateTime.Now;
+
     }
 }
